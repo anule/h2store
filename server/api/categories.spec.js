58ccda488b8395b4b4528d5bec4d9b1787d846bd
anule <anule.ndukwu@gmail.com>
2017-10-30 15:34:39 -0400

    write tests for categories and products routes

---
+++ server/api/categories.spec.js
@@ -0,0 +1,35 @@
+/* global describe beforeEach it */
+
+const { expect } = require('chai');
+const request = require('supertest');
+const db = require('../db');
+const app = require('../index');
+const Category = db.model('category');
+
+describe('Category routes', () => {
+  beforeEach(() => {
+    return db.sync({ force: true });
+  });
+
+  describe('/api/categories/', () => {
+    const sampleCategory = {
+      name: 'celebrity',
+      description: 'Hollywood drinking water'
+    };
+
+    beforeEach(() => {
+      return Category.create(sampleCategory);
+    });
+
+    it('GET /api/categories', () => {
+      return request(app)
+        .get('/api/categories')
+        .expect(200)
+        .then(res => {
+          expect(res.body).to.be.an('array');
+          expect(res.body[0].name).to.be.equal(sampleCategory.name);
+          expect(res.body[0].description).to.be.equal(sampleCategory.description);
+        });
+    });
+  }); // end describe('/api/categories')
+}); // end describe('Category routes')
---
+++ server/api/products.spec.js
@@ -0,0 +1,39 @@
+/* global describe beforeEach it */
+
+const { expect } = require('chai');
+const request = require('supertest');
+const db = require('../db');
+const app = require('../index');
+const Product = db.model('product');
+
+describe('Product routes', () => {
+  beforeEach(() => {
+    return db.sync({ force: true });
+  });
+
+  describe('/api/products/', () => {
+    const sampleProduct = {
+      name: 'male tears',
+      price: '$10.00',
+      description: ':( sad boys',
+      image: '',
+    };
+
+    beforeEach(() => {
+      return Product.create(sampleProduct);
+    });
+
+    it('GET /api/products', () => {
+      return request(app)
+        .get('/api/products')
+        .expect(200)
+        .then(res => {
+          expect(res.body).to.be.an('array');
+          expect(res.body[0].name).to.be.equal(sampleProduct.name);
+          expect(res.body[0].price).to.be.equal(sampleProduct.price);
+          expect(res.body[0].description).to.be.equal(sampleProduct.description);
+          expect(res.body[0].image).to.be.equal(sampleProduct.image);
+        });
+    });
+  }); // end describe('/api/products')
+}); // end describe( Product routes')
