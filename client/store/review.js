81fbbbc48187f59901e9752c1e39885869f2ed3d
Maria Betances <mbetances1002@gmail.com>
2017-11-05 00:35:32 -0400

    updated

--- client/store/review.js
+++ client/store/review.js
@@ -9,37 +9,15 @@ export const createReview = review => ({type: CREATE_REVIEW, review})
 export const getReview = reviews => ({type: GET_REVIEW, reviews})
 
 // THUNK CREATORS
-// export const postReview = (review) =>
-//   dispatch =>
-//     axios.post(`/api/products/${review.productId}/review`, review)
-//     .then(res => res.data)
-//     .then(newReview => dispatch(createReview(newReview)))
-//     .catch(err => console.error(`Creating review: unsuccessful`, err))
-//   }
-
 export function postReview (review) {
   return function thunk (dispatch) {
     return axios.post(`/api/products/${review.productId}/review`, review)
-      .then(res => res.data)
-      .then(newReview => {
-        dispatch(getReview(newReview));
-      });
+      .then(res => {
+        dispatch(getReview(res.data));
+      }
+    )
   };
 }
-
-
-// export function postChannel (channel, history) {
-//
-//   return function thunk (dispatch) {
-//     return axios.post('/api/channels', channel)
-//       .then(res => res.data)
-//       .then(newChannel => {
-//         dispatch(getChannel(newChannel));
-//         socket.emit('new-channel', newChannel);
-//         history.push(`/channels/${newChannel.id}`);
-//       });
-//   };
-// }
 
 // REDUCER
 export default function reducer(state = {}, action){
