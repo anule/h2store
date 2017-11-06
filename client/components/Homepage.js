48f4a60327ed5770b6c095a536d281046853fc22
anule <anule.ndukwu@gmail.com>
2017-11-06 12:36:09 -0500

    emulate wireframes basic homepage style structure

--- client/components/Homepage.js
+++ client/components/Homepage.js
@@ -18,19 +18,21 @@ const Homepage = () => {
         <span>Best Sellers</span>
         <BestSellers />
       </section>
-      <section id="our-difference">
+      <section>
         <h1>The h2Store Difference</h1>
-        <div className="difference">
-          <h2>Difference 1</h2>
-          <p>The most variety you can find online.</p>
-        </div>
-        <div className="difference">
-          <h2>Difference 2</h2>
-          <p>The higest quality water mingling with the most wretched and denigrated water, which is democracy.</p>
-        </div>
-        <div className="difference">
-          <h2>Difference 3</h2>
-          <p>Exceptional customer service that'll make you almost forget that you're being picky about which version of a chemically identical product you're ordering.</p>
+        <div id="our-difference">
+          <div className="difference">
+            <h2>Difference 1</h2>
+            <p>The most variety you can find online.</p>
+          </div>
+          <div className="difference">
+            <h2>Difference 2</h2>
+            <p>The higest quality water mingling with the most wretched and denigrated water, which is democracy.</p>
+          </div>
+          <div className="difference">
+            <h2>Difference 3</h2>
+            <p>Exceptional customer service that'll make you almost forget that you're being picky about which version of a chemically identical product you're ordering.</p>
+          </div>
         </div>
       </section>
       <hr />
