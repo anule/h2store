1c7270c8f586b0b65353723a2a4837f63b94d25f
Maria Betances <mbetances1002@gmail.com>
2017-11-06 12:53:11 -0500

    adding OrderHistory file

---
+++ client/components/OrderHistory.js
@@ -0,0 +1,12 @@
+// import React, { Component } from 'react'
+import {connect} from 'react-redux'
+
+function orderHistory (props){
+
+}
+
+const mapState = null
+
+const mapDispatch = null
+
+export default connect(mapState, mapDispatch)(orderHistory)
